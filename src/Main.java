645c8aa55334ade1ed0423952871d044d52746d1
USER <tasha2k7@mail.ru>
2016-05-11 20:52:19 +0200

    Added game "Guess the number".

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,27 @@ public class Main {
 
     public static void main(String...args) {
 
-        System.out.println("Welcome!");
         Scanner reader = new Scanner(System.in);
-        String input = reader.next();
-        System.out.println(input);
+
+        System.out.println("Welcome!\nNow you can enter your number:");
+
+        int targetNumber = 10;
+        int enteredNumber;
+
+        if (reader.hasNextInt()) {
+            enteredNumber = reader.nextInt();
+
+            if (enteredNumber == targetNumber) {
+                System.out.println(true);
+            } else {
+                System.out.println(false);
+            }
+
+        } else {
+            System.out.println(false);
+        }
+
+
 
     }
 
