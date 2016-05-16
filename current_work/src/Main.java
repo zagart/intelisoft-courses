d1392dc1928d92b42f0068fb1dde1fe2beee7ecb
USER <tasha2k7@mail.ru>
2016-05-16 20:51:26 +0200

    Example "even numbers".

--- current_work/src/Main.java
+++ current_work/src/Main.java
@@ -5,9 +5,7 @@ public class Main {
         int array[] = new int[10];
 
         for (int i = 0; i < array.length; i++) {
-
-            array[i] = i;
-
+            array[i] = (i + 1) * 2;
         }
 
         for (int element : array) {
