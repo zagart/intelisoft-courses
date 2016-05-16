a1f90af621f74fc56318c31e4331c7310ab2a849
Zagrebantsev Artiom <zagart.dev@mail.ru>
2016-05-16 21:35:12 +0200

    Example "Random + find max".

--- current_work/src/Main.java
+++ current_work/src/Main.java
@@ -1,17 +1,36 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String...args) {
 
-        int array[] = new int[10];
+        int x = 8;
+        int y = 5;
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (i + 1) * 2;
+        Random random = new Random();
+        int array[][] = new int[x][y];
+
+        int max = 0;
+        int p1 = 0;
+        int p2 = 0;
+
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = 10 + random.nextInt(90);
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                    p1 = i;
+                    p2 = j;
+                }
+                System.out.print(array[i][j] + "; ");
+            }
+            System.out.println();
         }
 
-        for (int element : array) {
-            System.out.println(element);
-        }
+
+        System.out.println("Answer: " + max + " p1 = " + p1 + "; p2 = " + p2 + ";");
 
     }
+
 
 }
