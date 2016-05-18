1096d24552b1336959cb260c6b7c52a6d44d12c3
Zagrebantsev Artiom <zagart.dev@mail.ru>
2016-05-18 19:45:23 +0200

    Example with Calendar committed. Class User created.

--- current_work/src/Main.java
+++ current_work/src/Main.java
@@ -1,34 +1,17 @@
-import java.util.Random;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Main {
 
     public static void main(String...args) {
 
-        int x = 8;
-        int y = 5;
+        Calendar cal = new GregorianCalendar(2010, Calendar.JULY, 10);
+        SimpleDateFormat format = new SimpleDateFormat(" d MMMM YYYY");
 
-        Random random = new Random();
-        int array[][] = new int[x][y];
-
-        int max = 0;
-        int p1 = 0;
-        int p2 = 0;
-
-        for (int i = 0; i < array[0].length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                array[i][j] = 10 + random.nextInt(90);
-                if (array[i][j] > max) {
-                    max = array[i][j];
-                    p1 = i;
-                    p2 = j;
-                }
-                System.out.print(array[i][j] + "; ");
-            }
-            System.out.println();
-        }
-
-
-        System.out.println("Answer: " + max + " p1 = " + p1 + "; p2 = " + p2 + ";");
+        System.out.println(format.format(cal.getTime()));
 
     }
 
---
+++ current_work/src/User.java
@@ -0,0 +1,8 @@
+/**
+ * Created by USER on 18.05.2016.
+ */
+public class User {
+
+
+
+}
