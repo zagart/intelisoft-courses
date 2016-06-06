b445fa6bfc1e62741d097ac8451f1cb21448cc40
weposaid <kon4ena.rega@gmail.com>
2016-06-06 21:39:00 +0300

    =Work with interfaces added. Lyambda test.

--- current_work/src/Main.java
+++ current_work/src/Main.java
@@ -1,40 +1,45 @@
-import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
+/**
+ * Created by intelisoft on 06.06.2016.
+ */
 public class Main {
 
     public static void main(String...args) {
+        User[] users = new User[3];
+        users[0] = new User("Nik", 30);
+        users[1] = new User("Den", 20);
+        users[2] = new User("Art", 10);
 
-        new LogicBlock();
+        CompareUserByAge comparator = new CompareUserByAge();
+        CompareUserByName comparator1 = new CompareUserByName();
 
-        A a = new A();
-        A a1 = new A();
+        //sort(users, (a, b) -> a.getLogin().compareToIgnoreCase(b.getLogin()));
+        printUsers(users, (user) -> System.out.printf("%s is %d years old. %n", user.getLogin(), user.getAge()));
 
-        StringBuilder sb = new StringBuilder();
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 100000000; i++) {
-            sb.append("a");
-        }
-z
-        long stop = System.currentTimeMillis();
-        System.out.println(stop - start + "time" );
-
-
-        a.setI(3);
-        a1.setI(4);
-        System.out.println(A.getI());
-
-        B b = new B();
-
-        User user = new User("login","password","email@mail.com");
-
-        System.out.println(user);
-        System.out.println(Math.sum(1,2));
-        ImmutablePoint ipoint = new ImmutablePoint(10,15);
-        ipoint.getY();
-        ipoint.getX();
-        System.out.println(ipoint);
+       // System.out.println(Arrays.asList(users));
 
     }
 
+    private static void sort (User[] users, ComparableUser comparator) {
+
+        for (int i = 0; i < users.length; i++) {
+            for (int j = 0; j < users.length - 1; j++) {
+                User a = users[j];
+                User b = users[j + 1];
+                if (comparator.compare(a, b) > 0) {
+                    users[j] = b;
+                    users[j + 1] = a;
+                }
+            }
+        }
+
+    }
+
+    private static void printUsers(User[] users, Printer printer) {
+        for (User user : users) {
+            printer.print(user);
+        }
+    }
 
 }
--- current_work/src/User.java
+++ current_work/src/User.java
@@ -3,7 +3,7 @@ import java.util.Date;
 /**
  * Created by USER on 18.05.2016.
  */
-public class User {
+public class User implements ComparableUser {
 
     private String login = "";
     private String password = "";
@@ -12,6 +12,12 @@ public class User {
     private String firstName = "";
     private String lastName = "";
     private String gender = "";
+    private int age = 0;
+
+    public User(String login, int age) {
+        this.login = login;
+        this.age = age;
+    }
 
     public User(String login, String password, String email) {
         this.login = login;
@@ -23,17 +29,18 @@ public class User {
     public String toString() {
         return "User{" +
                 "login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", gender='" + gender + '\'' +
+                ", age=" + age +
                 '}';
+    }
+
+    public String getLogin() {
+        return login;
     }
 
     public User(String login, String password, String email, String firstName, String lastName) {
         this.login = login;
         this.password = password;
+
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -46,6 +53,15 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int compare(User user1, User user2) {
+
+        return 0;
     }
 
 }
